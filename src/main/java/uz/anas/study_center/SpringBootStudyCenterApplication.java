34b46d25259aac6cdae8cb543f9dfd80b346a4ab
anas <azizortukov818@gmail.com>
2024-05-19 16:48:32 +0500

    initial commit

---
+++ src/main/java/uz/anas/study_center/SpringBootStudyCenterApplication.java
@@ -0,0 +1,13 @@
+package uz.anas.study_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/anas/study_center/SpringBootStudyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.anas.study_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
